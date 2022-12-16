78c2e4344badcf01b56795d51dbe6e4d2b8a6399
reondelo <reondelo@gmail.com>
2022-12-16 18:56:13 +0100

    add ShowItemComponent in HomePage

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BasicComponentModule } from 'src/app/components/basics/basics.components.module';
 import { SearchBoxModule } from 'src/app/components/search-box/search-box.module';
-import { StarsRatingModule } from 'src/app/components/stars-rating/start-rating.module';
+import { ShowItemModule } from 'src/app/components/show-item/show-item.module';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page.routing';
 
@@ -10,7 +10,7 @@ import { HomePageRoutingModule } from './home-page.routing';
   imports: [
     CommonModule,
     HomePageRoutingModule,
-    StarsRatingModule,
+    ShowItemModule,
     BasicComponentModule,
     SearchBoxModule,
   ],
