884d8026c95bebef8a97ba856fa68d765b3710f8
reondelo <reondelo@gmail.com>
2022-12-16 18:40:21 +0100

    add StarsRatingComponent in HomePage and ShowDetailPage

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BasicComponentModule } from 'src/app/components/basics/basics.components.module';
 import { SearchBoxModule } from 'src/app/components/search-box/search-box.module';
+import { StarsRatingModule } from 'src/app/components/stars-rating/start-rating.module';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page.routing';
 
@@ -9,6 +10,7 @@ import { HomePageRoutingModule } from './home-page.routing';
   imports: [
     CommonModule,
     HomePageRoutingModule,
+    StarsRatingModule,
     BasicComponentModule,
     SearchBoxModule,
   ],
--- src/app/pages/show-detail-page/show-detail-page.module.ts
+++ src/app/pages/show-detail-page/show-detail-page.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { ShowDetailPageRoutingModule } from './show-detail-page.routing';
 import { ShowDetailPageComponent } from './show-detail-page.component';
+import { StarsRatingModule } from 'src/app/components/stars-rating/start-rating.module';
 
 @NgModule({
-  imports: [CommonModule, ShowDetailPageRoutingModule],
+  imports: [CommonModule, ShowDetailPageRoutingModule, StarsRatingModule],
   declarations: [ShowDetailPageComponent],
 })
 export class ShowDetailPageModule {}
