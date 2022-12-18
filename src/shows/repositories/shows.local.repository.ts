093517b290295926726a33c1cdb9d967a4e95e12
reondelo <reondelo@gmail.com>
2022-12-18 20:51:08 +0100

    fix ShowsLocalRepository adding getSingleByName

--- src/shows/repositories/shows.local.repository.ts
+++ src/shows/repositories/shows.local.repository.ts
@@ -1,9 +1,20 @@
 import { SHOWS_MOCK } from '../mocks/shows.mock';
 import { Show } from '../show.model';
 import { ShowsRepository } from './shows.repository.model';
+import { Response } from '../response.model';
 
 export class ShowsLocalRepository implements ShowsRepository {
-  async getByName(name: string): Promise<Show[]> {
-    return SHOWS_MOCK.filter((s) => s.name.includes(name));
+  async getByName(name: string): Promise<Response<Show[]>> {
+    return {
+      result: SHOWS_MOCK.filter((s) => s.name.includes(name)),
+    };
+  }
+
+  async getSingleByName(name: string): Promise<Response<Show>> {
+    const { result, error } = await this.getByName(name);
+    return {
+      error,
+      result: result?.length ? result[0] : undefined,
+    };
   }
 }
